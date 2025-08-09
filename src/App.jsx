b8c2a1a226a7cf65eb2c6a974cb082af1b340e55
Rahul Ranjan <rahrranj@Rahuls-MacBook-Pro.local>
2025-08-09 11:13:44 +0530

    allowed rahul

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ export default function App() {
       const email = (decoded.email || '').toLowerCase();
 
       const allowed =
-        email.endsWith('@bmsce.ac.in') || email.endsWith('.bmsce.ac.in');
+        email.endsWith('@bmsce.ac.in') || email.endsWith('.bmsce.ac.in')|| email=="rahulranjan5sep@gmail.com";
 
       if (allowed) {
         setProfile(decoded);
