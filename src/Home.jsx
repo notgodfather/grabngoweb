105851f9de750625d94fb161b40397e6a8de7b85
notgodfather <ankitranjan10may@gmail.com>
2025-11-13 08:48:45 +0530

    alert msg

--- src/Home.jsx
+++ src/Home.jsx
@@ -404,8 +404,7 @@ function Header({ profile, search, onSearchChange, cartCount, onViewCart, accept
       <div style={hdrTopRowStyle}>
         <div style={hdrHelloStyle}>
           <span style={{ marginRight: 8 }}>👋</span>
-          <span style={{ fontWeight: 800 }}>Hi, {firstName} <br></span>
-          <span style={{fontWeight: 300, color: 'red'}}>Keep checking your order status in my orders section, once it's ready for pickup go and pick you order from VIDYARTHI KHANA</span>
+          <span style={{ fontWeight: 800 }}>Hi, {firstName}</span>
         </div>
         {/* Only show chip when ordering is paused */}
         {!acceptingOrders && (
